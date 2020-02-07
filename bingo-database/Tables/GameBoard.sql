1674fc8a7507c74b26eb8983a4a6acc130f0ed74
Heidenreich, Darl <darl.heidenreich@mutualofomahabank.com>
2020-02-07 02:30:51 -0700

    Add project files.

---
+++ bingo-database/Tables/GameBoard.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[GameBoard]
+(
+	[Id] BIGINT NOT NULL , 
+    [Title] NCHAR(20) NOT NULL, 
+    [Author] NCHAR(20) NOT NULL, 
+    [IsPublic] BIT NOT NULL, 
+    CONSTRAINT [PK_GameBoard] PRIMARY KEY ([Id])
+)
---
+++ bingo-database/Tables/GameTile.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[GameTile]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY, 
+    [GameBoardId] BIGINT NOT NULL, 
+    [Content] NCHAR(50) NOT NULL
+)
