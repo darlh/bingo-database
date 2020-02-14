ec940ddcb48c65017b51fff60fa8aff6452c25e5
Heidenreich, Darl <darl.heidenreich@mutualofomahabank.com>
2020-02-13 18:26:32 -0700

    tile content max

--- bingo-database/Tables/GameTile.sql
+++ bingo-database/Tables/GameTile.sql
@@ -2,7 +2,7 @@
 (
 	[Id] BIGINT NOT NULL , 
     [GameBoardId] BIGINT NOT NULL, 
-    [Content] NCHAR(50) NOT NULL, 
+    [Content] NVARCHAR(MAX) NOT NULL, 
     CONSTRAINT [PK_GameTile] PRIMARY KEY ([Id]),
     CONSTRAINT [FK_GameBoard] FOREIGN KEY ([GameBoardId]) REFERENCES [GameBoard]([Id])
 )
